04c510c2d65678a7d90638c099b8772d886e8e90
TiagoRibeiro09 <80395576+TiagoRibeiro09@users.noreply.github.com>
2021-03-22 21:18:20 +0000

    Initial commit

---
+++ Assets/scripts/Ball.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField]
+    float velocidade = 5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if (Random.value < 0.5f)
+        //{
+        //GetComponent<Rigidbody2D>().velocity = velocidade * Vector2.right;
+        //}else
+        //{ 
+        //GetComponent<Rigidbody2D>().velocity = velocidade * Vector2.right; 
+        //}
+        StartCoroutine(Ball());
+       
+        IEnumerator Ball()
+        {
+            yield return new WaitForSeconds(2f);
+            GetComponent<Rigidbody2D>().velocity = velocidade * Random.insideUnitSphere;
+        }
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+    }
+}
---
+++ Assets/scripts/Paddle.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField]
+    float velocidade = 0.5f;
+    [SerializeField]
+    KeyCode upKey = KeyCode.UpArrow;
+    [SerializeField]
+    KeyCode downKey = KeyCode.DownArrow;
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(upKey))
+        {
+            transform.position =
+                transform.position + velocidade * Vector3.up * Time.deltaTime;
+
+
+
+        }
+        else if (Input.GetKey(downKey))
+        {
+            transform.position =
+                transform.position + velocidade * Vector3.down * Time.deltaTime;
+        }
+
+        float cameraHeight = Camera.main.orthographicSize;
+        float halfPaddleSize = 0.5f;
+
+        // if (transform.position.y >= cameraHeight - halfPaddleSize)
+        // {
+        //Vector3 positionAux = transform.position;
+        //positionAux.y = cameraHeight - halfPaddleSize;
+        //transform.position = positionAux;
+        // }
+        //if (transform.position.y <= cameraHeight - halfPaddleSize)
+        // {
+        //Vector3 positionAux = transform.position;
+        // positionAux.y = cameraHeight + halfPaddleSize;
+        // transform.position = positionAux;
+        // }
+
+        Vector3 positionAux = transform.position;
+        positionAux.y = Mathf.Clamp(transform.position.y, -cameraHeight + halfPaddleSize, cameraHeight - halfPaddleSize);
+        transform.position = positionAux;
+
+       // if (transform.position.y >= cameraHeight * halfPaddleSize) //modo infinito de cima/baixo
+        //{
+            //Vector3 positionAux = transform.position;
+            //positionAux.y = cameraHeight - halfPaddleSize;
+           // transform.position = positionAux;
+        //}
+
+    }
+}
+
+
+
+
